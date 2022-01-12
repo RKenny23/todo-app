876e4689db5fa9d0c9df05b0c2fedaa9b5b4ea35
RKenny23 <rkenny238@hotmail.com>
2022-01-12 15:08:37 -0500

    css changes.

--- src/index.js
+++ src/index.js
@@ -43,7 +43,6 @@ tasksContainer.addEventListener("click", (e) => {
   }
 });
 
-
 clearCompleteTasksButton.addEventListener("click", (e) => {
   const selectedList = lists.find((list) => list.id === selectedListId);
   selectedList.tasks = selectedList.tasks.filter((task) => !task.complete);
